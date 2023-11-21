9214c9b4cb515429289bc450a1fd9a424cc6f99c
yassine errakkas <yassine.errakkas11@gmail.com>
2023-11-21 15:25:47 +0100

    first commit

---
+++ src/main/java/com/app/allergy_alert_be/AllergyAlertBeApplication.java
@@ -0,0 +1,13 @@
+package com.app.allergy_alert_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllergyAlertBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllergyAlertBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/allergy_alert_be/AllergyAlertBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.allergy_alert_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllergyAlertBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
