fd285b7ab99d3593f712015a63fd939dfa1d9371
yassine errakkas <yassine.errakkas11@gmail.com>
2023-11-25 17:30:10 +0100

    built an ocr model with python to conver the user provided image to a list of strings

--- ocr_module.py
+++ ocr_module.py
@@ -6,29 +6,19 @@ from io import BytesIO
 from PIL import Image
 from flask_cors import CORS
 
+#change this path with the tesseract executable path in your machine
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/extract_words', methods=["GET", 'POST'])
 def extract_words():
-    if request.method == 'GET':
-        return "hi"
-    if request.method == 'POST':
-        image = request.json['image']
-        if 'http' in image:
-            response = requests.get(image)
-            img = Image.open(BytesIO(response.content))
-            text = pytesseract.image_to_string(img)
-            words = text.split()
-            return jsonify({'words': words})
-        else:
-            image_bytes = base64.b64decode(image)
-            img = Image.open(BytesIO(image_bytes))
-            text = pytesseract.image_to_string(img)
-            words = text.split()
-            return jsonify({'words': words})
-
+    image = request.json['image']
+    image_bytes = base64.b64decode(image)
+    img = Image.open(BytesIO(image_bytes))
+    text = pytesseract.image_to_string(img)
+    words = text.split()
+    return jsonify({'words': words})
 
 if __name__ == '__main__':
     app.run(debug=True)
